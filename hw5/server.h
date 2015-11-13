f30c8b542d4d42d0e554f7739a5d1de0bbf16976
smsaleh <smsaleh@cs.stonybrook.edu>
2015-11-12 18:07:09 -0800

    i have no idea where to begin

---
+++ hw5/server.h
@@ -0,0 +1,12 @@
+//Devon & Shakeeb
+//aka yourefailingtryharder
+#ifndef __SERVER_H
+#define __SERVER_H
+  #include <stdio.h>
+  #include <string.h>
+  #include <unistd.h>
+  #include <stdlib.h>
+  #include <errno.h>
+
+
+#endif
