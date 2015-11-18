6789dedbf31704695acc4c8812a34a8c309a761e
dmaguire <dmaguire@cs.stonybrook.edu>
2015-11-17 23:41:01 -0500

    Added connection stuff and the beginning of threading.

--- hw5/server.c
+++ hw5/server.c
@@ -19,11 +19,11 @@ int main (int argc, char ** argv) {
         break;
       case 'h': // help flag
         hflag++;
-        printf("./server [-he] PORT_NUMBER MOTD\n
-        	-e\t\tEcho messages received on the server's stdout.\n
-        	-h\t\tDisplay the help menu & returns EXIT_SUCCESS\n
-        	PORT_NUMBER\t\tPort number to listen on.\n
-        	MOTD\t\tMessage to display on the client when they connect.\n");
+        printf("./server [-he] PORT_NUMBER MOTD\n"
+        	"-e\t\tEcho messages received on the server's stdout.\n"
+        	"-h\t\tDisplay the help menu & returns EXIT_SUCCESS\n"
+        	"PORT_NUMBER\t\tPort number to listen on.\n"
+        	"MOTD\t\tMessage to display on the client when they connect.\n");
         exit(EXIT_SUCCESS); // exit success
         break;
       default: // default do nothing
@@ -32,11 +32,11 @@ int main (int argc, char ** argv) {
   }
 
   motd = malloc(MAXMSG); // allocate space for message of the day
-  if(**argv[1] != 'e') { // check to see if the second arg contains an e flag
-  	port = argv[1];
+  if(*argv[1] != 'e') { // check to see if the second arg contains an e flag
+  	port = atoi(argv[1]);
   	motd = argv[2];
   } else { // if it does contain an e then move to next two
-  	port = argv[2];
+  	port = atoi(argv[2]);
   	motd = argv[3];
   }
 
@@ -44,15 +44,84 @@ int main (int argc, char ** argv) {
   printf("Currently listening on port %d.", port);
 
   /* Begin listening for clients */
-  int listen_fd, connfd;
+  struct epoll_event ev, events[10];
+  int listen_fd, *connfd, epollfd, numfd, i;
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
+  pthread_t tid;
+  //static pool pool;
 
+  // listen for clients
   listen_fd = open_listenfd(port);
-  while (1) {
-  	clientlen = sizeof(clientaddr); // TODO: Wrapper functions
-  	connfd = accept(listen_fd, (SA *)&clientaddr, &clientlen);
-  	// use epoll instead of select
+
+  // create epoll event to initailize the epoll event struct
+  epollfd = epoll_create(10);
+  ev.events = EPOLLIN; // set events so you can read & write
+  ev.data.fd = listen_fd;
+  if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_fd, &ev) == -1) {
+  	perror("epoll_ctl: listen_sock");
+  	exit(EXIT_FAILURE);
   }
 
+  while (1) {
+  	numfd = epoll_wait(epollfd, events, 10, -1);
+
+  	for (i = 0; i < numfd; i++) {
+  		if (events[i].data.fd == listen_fd) {
+  			// create copy of connection file descriptor and accept fd
+  			clientlen = sizeof(clientaddr); // TODO: Wrapper functions
+  			connfd = malloc(sizeof(int));
+  			*connfd = accept(listen_fd, (struct sockaddr *)&clientaddr, &clientlen);
+
+            ev.events = EPOLLIN | EPOLLOUT; // set ability to read/write to fd
+            ev.data.fd = *connfd;
+            epoll_ctl(epollfd, EPOLL_CTL_ADD, *connfd, &ev);
+
+            // create thread to login this fd
+            pthread_create(&tid, NULL, login_thread, &connfd);
+  		}
+  	}
+  }
+
+}
+
+int open_listenfd(int port) {
+	// opens socket connection
+	int listen_fd;
+	struct sockaddr_in serveraddr;
+
+	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	bzero(&serveraddr, sizeof(serveraddr));
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htons(INADDR_ANY);
+	serveraddr.sin_port = htons(port);
+
+	bind(listen_fd, (struct sockaddr *) &serveraddr, sizeof(serveraddr));
+
+	listen(listen_fd, 10);
+
+	return listen_fd;
+}
+
+void* login_thread(void *fd) {
+	int pfd = *((int *)fd);
+	pfd = pfd;
+	// code to login a client to the server
+	// first check to see if the client's name is already in use
+	// if yes then reject connection
+	// if no then connect with the client
+	// double check this pseudocode with the hw pdf
+	if (eflag == 1) {
+		// start echo thread
+		eflag++;
+	} else if (eflag == 2) {
+		// do nothing?
+	}
+
+	return NULL;
+}
+
+void* echo_thread(void *fd) {
+	// echo thread
 }
--- hw5/server.h
+++ hw5/server.h
@@ -1,16 +1,40 @@
 // Devon & Shakeeb
 // aka: YoureFailingTryHarder
 #ifndef __SERVER_H
-#define __SERVER_H
-  #include <stdio.h>
-  #include <string.h>
-  #include <unistd.h>
-  #include <stdlib.h>
-  #include <sys/socket.h>
-  #include <errno.h>
+	#define __SERVER_H
+	#include <stdio.h>
+	#include <string.h>
+	#include <unistd.h>
+	#include <stdlib.h>
+	#include <sys/socket.h>
+	#include <sys/epoll.h>
+	#include <pthread.h>
+	#include <sys/types.h>
+	#include <netdb.h>
+	#include <errno.h>
 
-#define MAXMSG 1001
-int port = 0, eflag = 0, hflag = 0;
-char *motd;
+	int open_listenfd(int port);
+	void *login_thread(void *pfd);
+	void* echo_thread(void *fd);
+
+	#define MAXMSG 1001
+	#define MAXNAME 1028
+	int port = 0, eflag = 0, hflag = 0;
+	char *motd;
+
+	/*typedef struct {
+		int maxfd;
+		fd_set read_set;
+		fd_set ready_set;
+		int nready;
+		int maxi;
+		int clientfd[2048];
+	} pool;
+
+	typedef struct client { // structure to hold client information
+		char username[MAXNAME];
+		char password[MAXNAME];
+		int fd;
+	};*/
 
 #endif
